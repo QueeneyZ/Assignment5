73e3b483f77e0a115f967b212b6d3f0a4dbe496e
QueeneyZ <QueeneyZ@outlook.com>
2025-09-17 00:08:48 +0930

    6

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,107 +7,72 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-    @R0  
-    M=0   //result = 0
 
-    // check if R1 < 0
-    @R1
-    D=M
-    @CHECK_R2
-    D;JNE
-    @END 
-    0;JMP
-
-(CHECK_R2)
-    // check if R2 == 0
-    @R2
-    D=M
-    @SETZERO
-    D;JEQ
-    @CONT 
-    0;JMP
-
-(SETZERO)
     @R0
-    M=0
-    @END
-    0;JMP
+    M=0        // R0 = 0
 
-(CONT)
-    // initialise sign = 0
     @Sign
-    M=0
+    M=0        // Sign = 0 (0 = positive, 1 = negative)
 
-    // if R1 < 0
+// Check R1 sign
     @R1
-    D=M 
+    D=M
     @R1_POS
-    D;JGE
-    D=-D
-    @R1 
-    M=D 
+    D;JGE      // if R1 >= 0, skip
+    D=-D       // else: make it positive
+    @R1
+    M=D
     @Sign
-    M=M+1
-(R1_POS)
+    M=M+1      // flip sign
 
-    // if R2 < 0
+(R1_POS)
+// Check R2 sign
     @R2
-    D=M 
+    D=M
     @R2_POS
     D;JGE
     D=-D
-    @R2 
-    M=D 
-    @Sign
-    M=M+1
-(R2_POS)
-
-    // Counter = R2
     @R2
-    D=M 
+    M=D
+    @Sign
+    M=M+1      // flip sign
+
+(R2_POS)
+// Counter = R2
+    @R2
+    D=M
     @Counter
     M=D
 
+// Loop: R0 += R1
 (LOOP)
     @Counter
     D=M
     @END_LOOP
-    D;JEQ
+    D;JEQ      // if counter == 0, end
 
     @R1
     D=M
-    @R0 
-    M=M+D 
+    @R0
+    M=M+D      // R0 += R1
 
     @Counter
-    M=M-1
+    M=M-1      // counter--
 
     @LOOP
     0;JMP
 
+// Negate result if needed
 (END_LOOP)
-    // If Sign == 1 -> negative
     @Sign
-    D=M 
-    @CHECK_TWO
-    D;JEQ
+    D=M
+    @NO_NEG
+    D;JEQ      // if Sign==0, result is positive → skip
+
     @R0
-    M=-M
-    @END
-    0;JMP 
+    M=-M       // else negate result
 
-(CHECK_TWO)
-    // if sign==2 (two negatives) -> positive, do nothing
-    @END
-    0;JMP
-
+(NO_NEG)
 (END)
     @END
-    0;JMP
-
-// --------- Variables ----------
-(Sign)
-    @0
-(Counter)
-    @0
-    
+    0;JMP      // infinite loop
